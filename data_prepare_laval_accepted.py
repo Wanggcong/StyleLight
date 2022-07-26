7b31c7acc42330798d300b3c9003169535a3b431
Wanggcong <1311328021@qq.com>
2022-07-26 09:14:02 +0000

    combined tonemapping

--- data_prepare_laval_accepted.py
+++ data_prepare_laval_accepted.py
@@ -10,40 +10,6 @@ from training.tonemapping import TonemapHDR
 pano_h=128
 pano_w=256
 
-'''
-class TonemapHDR(object):
-    """
-        Tonemap HDR image globally. First, we find alpha that maps the (max(numpy_img) * percentile) to max_mapping.
-        Then, we calculate I_out = alpha * I_in ^ (1/gamma)
-        input : nd.array batch of images : [H, W, C]
-        output : nd.array batch of images : [H, W, C]
-    """
-
-    def __init__(self, gamma=2.4, percentile=99, max_mapping=0.99):
-        self.gamma = gamma
-        self.percentile = percentile
-        self.max_mapping = max_mapping  # the value to which alpha will map the (max(numpy_img) * percentile) to
-
-    def __call__(self, numpy_img, clip=True, alpha=None, gamma=True):
-        if gamma:
-            power_numpy_img = np.power(numpy_img, 1 / self.gamma)
-        else:
-            power_numpy_img = numpy_img
-        non_zero = power_numpy_img > 0
-        if non_zero.any():
-            r_percentile = np.percentile(power_numpy_img[non_zero], self.percentile)
-        else:
-            r_percentile = np.percentile(power_numpy_img, self.percentile)
-        if alpha is None:
-            alpha = self.max_mapping / (r_percentile + 1e-10)
-        tonemapped_img = np.multiply(alpha, power_numpy_img)
-
-        if clip:
-            tonemapped_img = np.clip(tonemapped_img, 0, 1)
-
-        return tonemapped_img.astype('float32'), alpha
-'''
-
 def pre_process_data(src_file, to_folder, phase, file_txt):
     dst_folder = os.path.join(to_folder, phase)
     to_path_crop = to_folder+'test_crop'
